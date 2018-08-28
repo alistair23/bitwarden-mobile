afb89bab84f228bcbb31cbc27cda5fa7d10ee1cf
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-28 08:47:47 -0400

    rename UserPushNotification

--- src/App/Services/PushNotificationListener.cs
+++ src/App/Services/PushNotificationListener.cs
@@ -114,7 +114,7 @@ namespace Bit.App.Services
                     break;
                 case Enums.PushType.SyncCiphers:
                 case Enums.PushType.SyncVault:
-                    var cipherMessage = JsonConvert.DeserializeObject<SyncUserPushNotification>(data.Payload);
+                    var cipherMessage = JsonConvert.DeserializeObject<UserPushNotification>(data.Payload);
                     if(cipherMessage.UserId != _authService.UserId)
                     {
                         break;
@@ -122,7 +122,7 @@ namespace Bit.App.Services
                     _syncService.FullSyncAsync(true);
                     break;
                 case Enums.PushType.SyncSettings:
-                    var domainMessage = JsonConvert.DeserializeObject<SyncUserPushNotification>(data.Payload);
+                    var domainMessage = JsonConvert.DeserializeObject<UserPushNotification>(data.Payload);
                     if(domainMessage.UserId != _authService.UserId)
                     {
                         break;
@@ -130,7 +130,7 @@ namespace Bit.App.Services
                     _syncService.SyncSettingsAsync();
                     break;
                 case Enums.PushType.SyncOrgKeys:
-                    var orgKeysMessage = JsonConvert.DeserializeObject<SyncUserPushNotification>(data.Payload);
+                    var orgKeysMessage = JsonConvert.DeserializeObject<UserPushNotification>(data.Payload);
                     if(orgKeysMessage.UserId != _authService.UserId)
                     {
                         break;
@@ -138,7 +138,7 @@ namespace Bit.App.Services
                     _syncService.SyncProfileAsync();
                     break;
                 case Enums.PushType.LogOut:
-                    var logOutMessage = JsonConvert.DeserializeObject<SyncUserPushNotification>(data.Payload);
+                    var logOutMessage = JsonConvert.DeserializeObject<UserPushNotification>(data.Payload);
                     if(logOutMessage.UserId == _authService.UserId)
                     {
                         _authService.LogOut(null);
